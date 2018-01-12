d418d7d83fb6c1ad7403cd8251ab42a2da90108c
Matteo <matbat96@gmail.com>
2018-01-12 09:59:18 +0100

    Added get and post

--- routes.js
+++ routes.js
@@ -1,12 +1,29 @@
 const express = require('express'),
   bodyParser = require('body-parser');
+var uuid = require('uuid-v4');
 const routes = express.Router()
+const list = []
+var id = 0
 
 
-/*GET*/
+/*
+  GET
+  Return astronauts
+*/
 routes.get('/', function(req, res) {
   //TODO: check if there is a filter, and filter
-  res.json("test")
+  res.json(list)
+})
+
+/*
+  POST
+  add astronaut
+*/
+routes.post('/', function(req, res, next) {
+  const newAstronaut = req.body
+  newAstronaut.astronautID = uuid()
+  list.push(newAstronaut)
+  res.json(newAstronaut)
 })
 
 module.exports = routes
